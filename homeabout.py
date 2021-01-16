30a1ef3efe571f65c72b8de19706cf89052b9123
fymohamm <fymohamm@ucsc.edu>
2021-01-16 13:13:02 -0800

    Main Python Flask Page

---
+++ homeabout.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/tryourservice")
+def tryourservice():
+    return render_template('tryourservice.html')
+
+
+@app.route("/aboutus")
+def aboutus():
+    return render_template('aboutus.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
