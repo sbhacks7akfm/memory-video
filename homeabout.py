4040fe8a4babc6173119c6b3197450914083a138
fymohamm <fymohamm@ucsc.edu>
2021-01-16 23:04:16 -0800

    Upload button foor 1 image

--- homeabout.py
+++ homeabout.py
@@ -13,9 +13,22 @@ def tryourservice():
     return render_template('tryourservice.html')
 
 
+@app.route("/tryourservice", methods=['POST'])
+def upload_file():
+    uploaded_file = request.files.get('file')
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
+
+
 @app.route("/aboutus")
 def aboutus():
     return render_template('aboutus.html')
+
+
+@app.route("/maps")
+def maps():
+    return render_template('maps.html')
 
 
 if __name__ == "__main__":
